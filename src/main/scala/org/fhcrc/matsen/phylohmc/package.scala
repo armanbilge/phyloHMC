9e0a1cd0b0997f73887a3ceccdb3bf05e2231257
Arman Bilge <armanbilge@gmail.com>
2016-04-22 00:06:04 +1200

    Implement Patterns and SeqIO

---
+++ src/main/scala/org/fhcrc/matsen/phylohmc/package.scala
@@ -0,0 +1,12 @@
+package org.fhcrc.matsen
+
+import scala.collection.Bag
+
+package object phylohmc {
+
+  type Pattern = Map[Taxon, Int]
+  val Pattern = Map
+  type Patterns = Bag[Pattern]
+  val Patterns = Bag
+
+}
