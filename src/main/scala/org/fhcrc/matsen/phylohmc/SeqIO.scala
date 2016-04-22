ae65fbc6de3410a175e968fe538b3789bfacd9a0
Arman Bilge <armanbilge@gmail.com>
2016-04-22 19:59:02 +1200

    Make SeqIO more user-friendly

--- src/main/scala/org/fhcrc/matsen/phylohmc/SeqIO.scala
+++ src/main/scala/org/fhcrc/matsen/phylohmc/SeqIO.scala
@@ -1,11 +1,10 @@
 package org.fhcrc.matsen.phylohmc
 
-import java.io.File
 import scala.io.Source
 
 object SeqIO {
 
-  def fromFasta(file: File): Patterns = {
+  def parseFasta(file: String): Patterns = {
     val seqs = Source.fromFile(file).mkString.split(">").map(Source.fromString(_).getLines()).drop(1).map(s => (s.next, s.mkString)).toVector
     val taxa = seqs.map(Function.tupled((t, _) => new Taxon(t)))
     seqs(0)._2.indices.map(i =>
