a9f12b08c1ee7fa2937e80ba3b292107a7a3524c
Arman Bilge <armanbilge@gmail.com>
2016-04-29 14:32:36 +1200

    Fix VuLeapProg implementation

--- src/main/scala/org/fhcrc/matsen/phylohmc/VuLeapProg.scala
+++ src/main/scala/org/fhcrc/matsen/phylohmc/VuLeapProg.scala
@@ -1,25 +1,33 @@
 package org.fhcrc.matsen.phylohmc
 
-import spire.algebra.Field
+import spire.algebra.{Field, Order}
 import spire.syntax.field._
 import spire.syntax.order._
+
+import scala.annotation.tailrec
 
 trait VuLeapProg[R, N] extends PhyloHMC[R, N] {
 
   def leapprog(eps: R)(z: Z[R, N]): Z[R, N] = {
-    val (zp, e) = z.q.lengths.indices.view.map(solveForEps(z)).zipWithIndex.filter(_._1.isDefined).map(ei => (ei._1.get, ei._2)).filter(_._1 <= eps).sortWith(_._1 < _._1).foldLeft((z, Field[R].zero)) { (ze, ti) =>
-      val (z, e) = ze
-      val (t, i) = ti
-      val zp = leapfrog(t - e)(z)
-      val q = (if (zp.q.isInternal(i)) rng.nextInt(3) else 0) match {
-        case 0 => zp.q
-        case 1 => zp.q.nni(i, false)
-        case 2 => zp.q.nni(i, true)
+
+    @tailrec
+    def recurse(z: Z[R, N], eps: R): Z[R, N] = {
+      z.q.lengths.indices.view.map(solveForEps(z)).zipWithIndex.filter(_._1.isDefined).map(ei => (ei._1.get, ei._2)).filter(_._1 <= eps).reduceOption(Order.by[(R, Int), R](_._1).min) match {
+        case Some((e, i)) =>
+          val zp = leapfrog(e)(z)
+              val q = (if (zp.q.isInternal(i)) rng.nextInt(3) else 0) match {
+                case 0 => zp.q
+                case 1 => zp.q.nni(i, false)
+                case 2 => zp.q.nni(i, true)
+              }
+              val p = z.p.updated(i, -z.p(i))
+              recurse(zp.copy(q = q, p = p)(U(q), K(p)), eps - e)
+        case None => leapfrog(eps)(z)
       }
-      val p = z.p.updated(i, -z.p(i))
-      (zp.copy(q = q, p = p)(U(q), K(p)), t)
     }
-    leapfrog(eps - e)(zp)
+
+    recurse(z, eps)
+
   }
 
 }
