c5a25e67dac38970057f620d7252958345242e61
Arman Bilge <armanbilge@gmail.com>
2016-05-06 15:50:09 +1200

    Implement non-zero barrier for pendant edges

--- src/main/scala/org/fhcrc/matsen/phylohmc/ReflectiveLeapFrog.scala
+++ src/main/scala/org/fhcrc/matsen/phylohmc/ReflectiveLeapFrog.scala
@@ -1,20 +1,22 @@
 package org.fhcrc.matsen.phylohmc
 
-import spire.algebra.Signed
+import spire.algebra.{Field, Signed}
 import spire.std.seq._
 import spire.syntax.vectorSpace._
 import spire.syntax.order._
 
 trait ReflectiveLeapFrog[R, N] extends PhyloHMC[R, N] {
 
+  val barrier = Field[R].fromDouble(0.0)
+
   def leapprog(eps: R)(z: Z[R, N]): Z[R, N] = {
     val halfEps = eps / 2
     val pp = z.p - halfEps *: z.dU
     val (_, dK) = K(pp)
     val qp = z.q.modifyLengths(_ + eps *: dK)
-    val qpp = qp.modifyLengths(_.map(Signed[R].abs))
+    val qpp = qp.modifyLengths(_.zipWithIndex.map(Function.tupled((li, i) => if (qp.isInternal(i)) Signed[R].abs(li) else Signed[R].abs(li - barrier) + barrier)))
     val Up = U(qpp)
-    val ppp = (qp.lengths, pp).zipped.map((qi, pi) => if (qi < 0) -pi else pi) - halfEps *: Up._2
+    val ppp = (qp.lengths, pp, pp.indices).zipped.map((qi, pi, i) => if (qi < (if (qp.isInternal(i)) Field[R].zero else barrier)) -pi else pi) - halfEps *: Up._2
     Z(qpp, ppp)(Up, K(ppp))
   }
 
--- src/main/scala/org/fhcrc/matsen/phylohmc/ReflectiveLeapProg.scala
+++ src/main/scala/org/fhcrc/matsen/phylohmc/ReflectiveLeapProg.scala
@@ -8,12 +8,16 @@ import spire.syntax.order._
 
 trait ReflectiveLeapProg[R, N] extends PhyloHMC[R, N] {
 
+  val barrier = Field[R].fromDouble(0.0)
+
   def leapprog(eps: R)(z: Z[R, N]): Z[R, N] = {
     val halfEps = eps / 2
     val pp = z.p - halfEps *: z.dU
     val Kp = K(pp)
-    val qp = z.q.modifyLengths(l => (l + eps *: Kp._2).map(Signed[R].abs))
-    val zp = (z.q.lengths, Kp._2).zipped.map(- _ / _).zipWithIndex.filter(Function.tupled((e, _) => Field[R].zero <= e && e <= eps)).sortBy(_._1).view.map(_._2).foldLeft(z.copy(q = qp, p = pp)(U(qp), Kp)) { (z, i) =>
+    val qp = z.q.modifyLengths(l => l + eps *: Kp._2)
+    val qpp = qp.modifyLengths(l => l.zipWithIndex.map(Function.tupled((li, i) => if (qp.isInternal(i)) Signed[R].abs(li) else Signed[R].abs(li - barrier) + barrier)))
+    val ppp = (qp.lengths, pp, pp.indices).zipped.map((qi, pi, i) => if (qp.isInternal(i)) pi else if (qi < barrier) -pi else pi)
+    val zp = (z.q.lengths, Kp._2).zipped.map(- _ / _).zipWithIndex.filter(Function.tupled((_, i) => z.q.isInternal(i))).filter(Function.tupled((e, _) => Field[R].zero <= e && e <= eps)).sortBy(_._1).view.map(_._2).foldLeft(z.copy(q = qpp, p = ppp)(U(qpp), K(ppp))) { (z, i) =>
       val q = (if (z.q.isInternal(i)) rng.nextInt(1) else 0) match {
         case 0 => z.q
         case 1 => z.q.nni(i, false)
@@ -22,8 +26,8 @@ trait ReflectiveLeapProg[R, N] extends PhyloHMC[R, N] {
       val p = z.p.updated(i, -z.p(i))
       z.copy(q, p)(U(q), K(p))
     }
-    val ppp = zp.p - halfEps *: zp.dU
-    zp.copy(p = ppp)(_K = K(ppp))
+    val pppp = zp.p - halfEps *: zp.dU
+    zp.copy(p = pppp)(_K = K(pppp))
   }
 
 }
