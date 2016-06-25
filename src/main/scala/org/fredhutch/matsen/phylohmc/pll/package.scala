bdc4023cc3892b95a38665c1a6fbea703872f276
Arman Bilge <armanbilge@gmail.com>
2016-06-25 10:41:48 -0400

    Pass operations as pointer, not array

--- src/main/scala/org/fredhutch/matsen/phylohmc/pll/package.scala
+++ src/main/scala/org/fredhutch/matsen/phylohmc/pll/package.scala
@@ -10,7 +10,7 @@ package object pll {
 
   private[pll] trait PartitionPointer extends Structure.ByReference
 
-  private[pll] class Operation(parent_clv_index: Int, parent_scaler_index: Int, child1_clv_index: Int, child1_matrix_index: Int, child1_scaler_index: Int, child2_clv_index: Int, child2_matrix_index: Int, child2_scaler_index: Int) extends Structure with Structure.ByValue {
+  private[pll] class OperationPointer(parent_clv_index: Int, parent_scaler_index: Int, child1_clv_index: Int, child1_matrix_index: Int, child1_scaler_index: Int, child2_clv_index: Int, child2_matrix_index: Int, child2_scaler_index: Int) extends Structure with Structure.ByReference {
     override def getFieldOrder: util.List[_] = util.Arrays.asList("parent_clv_index", "parent_scaler_index", "child1_clv_index", "child1_matrix_index", "child1_scaler_index", "child2_clv_index", "child2_matrix_index", "child2_scaler_index")
   }
 
@@ -34,7 +34,7 @@ package object pll {
 
   @native private[pll] def pll_update_invariant_sites_proportion(partition: PartitionPointer, params_index: Int, prop_invar: Double): Int
 
-  @native private[pll] def pll_update_partials(partition: PartitionPointer, operations: Array[Operation], count: Int): Double
+  @native private[pll] def pll_update_partials(partition: PartitionPointer, operations: OperationPointer, count: Int): Double
 
   @native private[pll] def pll_compute_root_loglikelihood(partition: PartitionPointer, clv_index: Int, scaler_index: Int, freqs_index: Array[Int], persite_lnl: Array[Double]): Double
 
