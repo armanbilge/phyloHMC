2a74e4602900a1beaedcedfac8775c269ce6b561
Arman Bilge <armanbilge@gmail.com>
2016-07-04 16:34:34 +1000

    Forgot "import" and style fix

--- src/main/scala/org/fredhutch/matsen/phylohmc/Main.scala
+++ src/main/scala/org/fredhutch/matsen/phylohmc/Main.scala
@@ -7,6 +7,6 @@ import tools.reflect.ToolBox
 object Main extends App {
 
   val toolBox = currentMirror.mkToolBox()
-  toolBox.compile(toolBox.parse(getClass().getPackage().getName() + "._\n" + Source.fromFile(args(0)).getLines().mkString("\n")))()
+  toolBox.compile(toolBox.parse("import " + getClass.getPackage.getName + "._\n" + Source.fromFile(args(0)).getLines().mkString("\n")))()
 
 }
