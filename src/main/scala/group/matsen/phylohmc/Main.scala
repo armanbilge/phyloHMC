356e83366b71dee0bcf0b1b3a51e69574af209e4
Arman Bilge <armanbilge@gmail.com>
2017-01-03 20:02:18 -0800

    Throw exceptions encountered while running script

--- src/main/scala/group/matsen/phylohmc/Main.scala
+++ src/main/scala/group/matsen/phylohmc/Main.scala
@@ -3,9 +3,18 @@ package group.matsen.phylohmc
 import java.io.File
 
 import ammonite.ops.Path
+import ammonite.util.Res.{Exception, Failure}
 
 object Main extends App {
 
-  ammonite.Main(predef = "import " + getClass.getPackage.getName + "._", verboseOutput = false).runScript(Path(new File(args(0)).getAbsolutePath), Seq.empty, Seq.empty)
+  ammonite.Main(predef = "import " + getClass.getPackage.getName + "._", verboseOutput = false).runScript(Path(new File(args(0)).getAbsolutePath), Seq.empty, Seq.empty) match {
+    case Failure(ex, msg) =>
+      ex match {
+        case Some(t) => throw t
+        case None => System.err.println(msg)
+      }
+    case Exception(t, msg) => throw t
+    case _ =>
+  }
 
 }
